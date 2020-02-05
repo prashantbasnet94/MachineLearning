3aa028259f751cb1635fc2d1eecb6e9da3335648
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-02-05 15:02:56 -0600

    data preprocessing

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -8,6 +8,12 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
+
+
+
+
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
  
 #Taking care of missing data
