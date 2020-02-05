894dd1bde1a17943690ece512e9e9f944d6de0b7
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-02-05 14:50:32 -0600

    Setting up files

---
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,17 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+ 
+#Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values ='NaN', strategy= 'mean', axis=0 )
+imputer =imputer.fit(x[:,1:3])
+x[:,1:3]=imputer.transform(x[:,1:3])
