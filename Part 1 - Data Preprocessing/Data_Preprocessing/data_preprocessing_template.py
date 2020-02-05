4386bef2f591b825a9b82f771ef3e4fc4f32effc
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2020-02-05 15:13:29 -0600

    Encoding variable name for category
    but the problem is if [0,1,2] 2 multiplied with equation is always greater than 1 and so 0

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -8,16 +8,28 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
-
-
-
-
-dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
+
+
+
+#read dataset of csv
+dataset = pd.read_csv('Data.csv')
+
+#store all values from all row and column expect for the last cloumn
+x = dataset.iloc[:,:-1].values
+#store all values from  all row but from only third cloumn
+y = dataset.iloc[:,3].values
+
  
-#Taking care of missing data
+#taking care of missing data
+
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values ='NaN', strategy= 'mean', axis=0 )
-imputer =imputer.fit(x[:,1:3])
-x[:,1:3]=imputer.transform(x[:,1:3])
+imputer  = Imputer(missing_values='NaN',strategy = 'mean', axis =0)
+imputer  = imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+
+
+#Taking care of missing data
+from sklearn.preprocessing import LabelEncoder
+labelEncoder = LabelEncoder()
+x[:,0]=labelEncoder.fit_transform(x[:,0])
